4b2610515fcb7549c261d6d2cddd93313cd49f3d
Pierre Lulé <pierre@lule.fr>
2012-09-22 13:17:58 +0200

    geometry

--- game.lua
+++ game.lua
@@ -1,18 +1,26 @@
+Camera = require 'hump.camera'
+Level = require 'level'
+
 local game = Gamestate.new()
 
 function game:init()
-
 end
 
-function game:load()
-
+function game:enter()
+	self.camera = Camera(Width/2,Height/2, 1, 0)
+	self.level = Level(Width/2, Height/2, 0)
+	self.nextLevel = Level(100, 100, 1, 0.1)
 end
 
 function game:update(dt)
-
+	self.nextLevel:setPosition(love.mouse.getPosition())
 end
 
 function game:draw()
+	self.camera:attach()
+	self.level:draw()
+	self.nextLevel:draw()
+	self.camera:detach()
 	love.graphics.print("A New World", 10, 10)
 end
 
---
+++ level.lua
@@ -0,0 +1,31 @@
+Class = require 'hump.class'
+
+local Level = Class
+{	name = "Level",
+	function(self, x, y, level, scale)
+		self.sizeX = 1024
+		self.sizeY = 768
+		self.x = x
+		self.y = y
+		self.scale = scale or 1 -- STABLE
+		self.level = level
+	end
+}
+
+function Level:draw()
+	love.graphics.setColor(self.level*10, self.level*10, self.level*10)
+	local sizeX,sizeY = self.sizeX*self.scale, self.sizeY*self.scale
+	local x = self.x-sizeX/2
+	local y = self.y-sizeY/2
+
+	love.graphics.rectangle("fill", x, y, sizeX, sizeY)
+	love.graphics.setColor(255,0,0)
+	love.graphics.setLine(2, "smooth")
+	love.graphics.rectangle("line", x, y, sizeX, sizeY)
+end
+
+function Level:setPosition(x,y)
+	self.x,self.y = x,y
+end
+
+return Level
